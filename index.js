9af297788bf6c11cd5fde94104ba9454d7a473ac
deepak31098 <110996070+deepak31098@users.noreply.github.com>
2022-09-20 19:09:13 +0530

    Second commit

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
-document.getElementsByClassName("footer")[0].addEventListener("mouseover",function(){
+var state=true;
+$(".share-box").click(function(){
   var wide=window.innerWidth;
-  if(wide<610){
-  document.getElementsByClassName("social-mobile")[0].classList.remove("invisible");
-  document.getElementsByClassName("footer")[0].classList.add("invisible");
-  document.querySelectorAll(".share-icon img")[0].classList.add("share-icon-hover");
-  document.getElementsByClassName("container-two")[0].classList.add("container-two-scroll-modification")
-}});
-
-
-document.getElementsByClassName("footer")[0].addEventListener("mouseout",function(){
+  if(wide>610 && state){
+  $(".social-desktop")[0].classList.remove("invisible");
+  $(".triangle")[0].classList.remove("invisible");
+  $(".share-box img")[0].classList.add("share-icon-hover");
+  $(".share-box")[0].classList.add("share-box-click")
+  state=false;
+}
+else if(wide>610 && !state){
+  $(".social-desktop")[0].classList.add("invisible");
+  $(".triangle")[0].classList.add("invisible");
+  $(".share-box img")[0].classList.remove("share-icon-hover");
+  $(".share-box")[0].classList.remove("share-box-click")
+  state=true;
+}
+else if(wide<610 && state){
+  $(".social-mobile")[0].classList.remove("invisible");
+  $(".footer")[0].classList.add("invisible");
+  $(".share-icon img")[0].classList.add("share-icon-hover");
+  $(".container-two")[0].classList.add("container-two-scroll-modification")
+  state=false;
+}
+})
+$(".share-icon").click(function(){
   var wide=window.innerWidth;
-  if(wide<610){
-  document.getElementsByClassName("social-mobile")[0].classList.add("invisible");
-  document.getElementsByClassName("footer")[0].classList.remove("invisible");
-  document.getElementsByClassName("container-two")[0].classList.remove("container-two-scroll-modification")
-}});
-
-document.getElementsByClassName("share-box")[0].addEventListener("mouseover",function(){
-  var wide=window.innerWidth;
-  if(wide>610){
-  document.getElementsByClassName("social-desktop")[0].classList.remove("invisible");
-  document.getElementsByClassName("triangle")[0].classList.remove("invisible");
-  document.querySelectorAll(".share-box img")[0].classList.add("share-icon-hover");
-}});
-
-
-document.getElementsByClassName("footer")[0].addEventListener("mouseout",function(){
-  var wide=window.innerWidth;
-  if(wide>610){
-  document.getElementsByClassName("social-desktop")[0].classList.add("invisible");
-  document.getElementsByClassName("triangle")[0].classList.add("invisible");
-  document.querySelectorAll(".share-box img")[0].classList.remove("share-icon-hover");
-}});
+  if(wide<610 && !state){
+  $(".social-mobile")[0].classList.add("invisible");
+  $(".footer")[0].classList.remove("invisible");
+  $(".share-icon img")[0].classList.remove("share-icon-hover");
+  $(".container-two")[0].classList.remove("container-two-scroll-modification")
+  state=true;
+  }
+});
